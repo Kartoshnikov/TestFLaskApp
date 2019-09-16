3276c57a35454b5fdd595b684ef137374ae4339a
Dmitriy_Kartoshnikov <kartoshnikov16@gmail.com>
2019-09-16 14:50:49 +0000

    docker-compose.yml added. app.by visits_c fixed

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ def hello():
     try:
         visits_c = redis.incr("counter")
     except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        visits_c = "<i>cannot connect to Redis, counter disabled</i>"
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
 
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits_c)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
